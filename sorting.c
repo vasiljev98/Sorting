4ce12f648ac5b6d1cba62d6c86e0560c14a07a70
Данил <vasiljev98@icloud.com>
2017-04-13 18:46:17 +0300

    откомментирован merge_sort

--- sorting.c
+++ sorting.c
@@ -121,27 +121,27 @@ void shell_sort (int *a, int n) {
     }
 };
 
-// void merge (int *a, int n, int m) {
-//     int i, j, k;
-//     int *x = malloc(n * sizeof (int));
-//     for (i = 0, j = m, k = 0; k < n; k++) {
-//         x[k] = j == n      ? a[i++]
-//              : i == m      ? a[j++]
-//              : a[j] < a[i] ? a[j++]
-//              :               a[i++];
-//     }
-//     for (i = 0; i < n; i++) {
-//         a[i] = x[i];
-//     }
-//     free(x);
-// };
+void merge (int *a, int n, int m) {
+    int i, j, k;
+    int *x = malloc(n * sizeof (int));
+    for (i = 0, j = m, k = 0; k < n; k++) {
+        x[k] = j == n      ? a[i++]
+             : i == m      ? a[j++]
+             : a[j] < a[i] ? a[j++]
+             :               a[i++];
+    }
+    for (i = 0; i < n; i++) {
+        a[i] = x[i];
+    }
+    free(x);
+};
  
-// void merge_sort (int *a, int n) {
-//     if (n < 2)
-//         return;
-//     int m = n / 2;
-//     merge_sort(a, m);
-//     merge_sort(a + m, n - m);
-//     merge(a, n, m);
-// };
+void merge_sort (int *a, int n) {
+    if (n < 2)
+        return;
+    int m = n / 2;
+    merge_sort(a, m);
+    merge_sort(a + m, n - m);
+    merge(a, n, m);
+};
 
